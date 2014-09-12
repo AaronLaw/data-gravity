25bebba34e895d8aa06929f152f6a87e80a58204
Matthew Makai <makaimc@gmail.com>
2014-09-11 22:24:10 -0700

    working on deployment process

--- deploy/run_playbook.sh
+++ deploy/run_playbook.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-ansible-playbook datagravity --private-key=./ssh_conf/id_rsa -K -u deployer -i ./hosts
+ansible-playbook datagravity.yml --private-key=./ssh_conf/id_rsa -K -u deployer -i ./hosts
