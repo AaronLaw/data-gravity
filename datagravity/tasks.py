da2771ec2ce93186818d21a7c4ca06f38ee4a619
Matthew Makai <makaimc@gmail.com>
2014-09-29 08:25:10 -0700

    adding function to count words in markdown files

--- datagravity/tasks.py
+++ datagravity/tasks.py
@@ -1,6 +1,8 @@
 import requests
 from datetime import timedelta
 from celery.decorators import periodic_task
+import sh
+from sh import cd, find, wc, cat
 
 from .models import Follower, Service
 from datagravity import app, db, celery
@@ -19,4 +21,22 @@ def github_follower_count(username):
         db.session.add(f)
         db.session.commit()
     return resp.status_code
+
+@celery.task
+def get_wc(content_dir):
+    """
+    """
+    filetype = "*.markdown"
+    cd(content_dir)
+    files_list = find(".", "-name", "*.markdown")
+    files_arr = files_list.split('\n')
+    word_count = 0
+    for f in files_arr:
+        if f:
+            try:
+                file_word_count = int(wc(cat(content_dir + f), "-w"))
+                word_count += file_word_count
+            except:
+                pass
+    return word_count
 
